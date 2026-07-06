11ca3b728ee488273aa33dbe42941799335b4ad5
Ryder949 <mattydunmall5@gmail.com>
2026-07-07 02:35:19 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,27 @@
-const CACHE_NAME = "loop-timer-v1";
+const CACHE_NAME = "loop-timer-v2";
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-    );
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+
+);
+
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+
+event.respondWith(
+
+caches.match(event.request)
+.then(response => {
+
+return response ||
+fetch(event.request);
+
+})
+
+);
+
 });
