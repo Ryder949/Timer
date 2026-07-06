ccd6cdb73f9043932de1a8f07ce618605e5f16d5
Ryder949 <mattydunmall5@gmail.com>
2026-07-07 01:39:13 +1000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = "loop-timer-v1";
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
